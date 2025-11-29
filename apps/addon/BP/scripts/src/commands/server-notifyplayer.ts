7008b06ad7e30727df05fc73fb81c17289cb6caf
dakoyo <dakoyo@outlook.jp>
2025-11-29 16:16:12 +0900

    arranged mcui

--- apps/addon/BP/scripts/src/commands/server-notifyplayer.ts
+++ apps/addon/BP/scripts/src/commands/server-notifyplayer.ts
@@ -1,5 +1,6 @@
 import * as mc from "@minecraft/server";
 import { debug } from "../setting";
+import { ActionFormData } from "@minecraft/server-ui";
 const { world, system } = mc;
 
 export const command: mc.CustomCommand = {
@@ -31,6 +32,23 @@ export function execute(ev: mc.CustomCommandOrigin, ...args: any[]): mc.CustomCo
         if (!player) {
             return;
         }
+
+        const form = new ActionFormData();
+        form.title("Proximity Voice Chat");
+        form.body([
+            "Room ID: §a" + args[1],
+            "Player Code: §a" + args[2]
+        ].join("§r\n"));
+        form.button("§aOK");
+        form.show(player).then(r => {
+            player.sendMessage([
+                "§l====================",
+                "§aProximity Voice Chat",
+                "§7Room ID: §a" + args[1],
+                "§7Player Code: §a" + args[2],
+                "§l===================="
+            ].join("§r\n"))
+        })
     })
 
     return {
